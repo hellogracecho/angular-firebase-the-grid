fd0bbdb77b84c187cfab2a04be84686912b7835f
Grace Cho <hellogracec@gmail.com>
2020-01-27 14:30:47 -0800

    firebase hosting added, deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCbt5N_Z44pT1hdGf18dD_uwtYnU7RNcE0',
-    authDomain: 'angular-firebase-grid-dash.firebaseapp.com',
-    databaseURL: 'https://angular-firebase-grid-dash.firebaseio.com',
-    projectId: 'angular-firebase-grid-dash',
-    storageBucket: 'angular-firebase-grid-dash.appspot.com',
-    messagingSenderId: '985074974658'
+    apiKey: "AIzaSyCs3Eh5Jjgd3pVavTk8-L5LoBESE28VQIA",
+    authDomain: "friendlychat-4f368.firebaseapp.com",
+    databaseURL: "https://friendlychat-4f368.firebaseio.com",
+    projectId: "friendlychat-4f368",
+    storageBucket: "friendlychat-4f368.appspot.com",
+    appId: "1:304137450569:web:857b0f3668211e801b7b98"
   }
 };
